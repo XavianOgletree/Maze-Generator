eb328a633e0ac8ea3fe30d37464c97e75c9affd3
Capital Ex <capitalex@protonmail.com>
2018-04-21 14:20:48 -0400

    (probably) Prevented invalid input

--- js/main.js
+++ js/main.js
@@ -3,10 +3,14 @@ function run() {
     let inputs = {
         width: document.getElementById("width"),
         height: document.getElementById("height"),
-        scale: document.getElementById("scale")
+        scale: document.getElementById("scale"),
+        validate: (input) => !Number.isInteger(Number.parseInt(input.value, 10)) || Number.parseInt(input.value) < Number.parseInt(input.min)
     };
     let button = document.getElementById("generate");
     drawMaze(generateMaze(20, 20), 5, canvas);
+    inputs.width.onchange = () => button.disabled = inputs.validate(inputs.width);
+    inputs.height.onchange = () => button.disabled = inputs.validate(inputs.height);
+    inputs.scale.onchange = () => button.disabled = inputs.validate(inputs.scale);
     button.onclick = () => {
         let width = Number.parseInt(inputs.width.value);
         let height = Number.parseInt(inputs.height.value);
--- ts/main.ts
+++ ts/main.ts
@@ -3,14 +3,21 @@ function run() {
     let inputs = {
         width: document.getElementById("width") as HTMLInputElement,
         height: document.getElementById("height") as HTMLInputElement,
-        scale: document.getElementById("scale") as HTMLInputElement
+        scale: document.getElementById("scale") as HTMLInputElement,
+        validate:  (input: HTMLInputElement) => !Number.isInteger(Number.parseInt(input.value, 10)) || Number.parseInt(input.value) < Number.parseInt(input.min)
     }
     let button = document.getElementById("generate") as HTMLButtonElement;
     drawMaze(generateMaze(20, 20), 5, canvas);
+    
+    inputs.width.onchange = () => button.disabled = inputs.validate(inputs.width);
+    inputs.height.onchange = () => button.disabled = inputs.validate(inputs.height);
+    inputs.scale.onchange = () => button.disabled = inputs.validate(inputs.scale);
+    
     button.onclick = () => {
         let width = Number.parseInt(inputs.width.value);
         let height = Number.parseInt(inputs.height.value);
         let scale = Number.parseInt(inputs.scale.value);
         drawMaze(generateMaze(width, height), scale, canvas);
     }
+
 }
