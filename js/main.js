e407245515cab86996bc9de9fdee5ced77383e3b
Capital Ex <capitalex@protonmail.com>
2018-04-26 22:56:26 -0400

    Added live mode to maze generator

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
-function run() {
+function initialize() {
     let canvas = document.getElementById("maze");
     let inputs = {
         width: document.getElementById("width"),
         height: document.getElementById("height"),
         scale: document.getElementById("scale"),
+        live: document.getElementById("live"),
         validate: (input) => !Number.isInteger(Number.parseInt(input.value, 10)) || Number.parseInt(input.value) < Number.parseInt(input.min)
     };
     let button = document.getElementById("generate");
-    drawMaze(generateMaze(20, 20), 5, canvas);
+    let height = 20;
+    let width = 20;
+    let state = {
+        iter: primIter(Graph.random(width, height)),
+        id: -1,
+    };
+    state.id = setInterval(() => {
+        let step = state.iter.next();
+        if (!step.done)
+            drawMaze(step.value, height, width, 5, canvas);
+        else
+            clearInterval(state.id);
+    }, 1);
     inputs.width.onchange = () => button.disabled = inputs.validate(inputs.width);
     inputs.height.onchange = () => button.disabled = inputs.validate(inputs.height);
     inputs.scale.onchange = () => button.disabled = inputs.validate(inputs.scale);
@@ -15,6 +28,19 @@ function run() {
         let width = Number.parseInt(inputs.width.value);
         let height = Number.parseInt(inputs.height.value);
         let scale = Number.parseInt(inputs.scale.value);
-        drawMaze(generateMaze(width, height), scale, canvas);
+        clearInterval(state.id);
+        if (!inputs.live.checked) {
+            drawMaze(prim(Graph.random(width, height)), width, height, scale, canvas);
+        }
+        else {
+            state.iter = primIter(Graph.random(width, height));
+            state.id = setInterval(() => {
+                let step = state.iter.next();
+                if (!step.done)
+                    drawMaze(step.value, height, width, scale, canvas);
+                else
+                    clearInterval(state.id);
+            }, 1);
+        }
     };
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,14 +1,28 @@
-function run() {
+function initialize() {
     let canvas = document.getElementById("maze") as HTMLCanvasElement;
     let inputs = {
         width: document.getElementById("width") as HTMLInputElement,
         height: document.getElementById("height") as HTMLInputElement,
         scale: document.getElementById("scale") as HTMLInputElement,
+        live: document.getElementById("live") as HTMLInputElement,
         validate:  (input: HTMLInputElement) => !Number.isInteger(Number.parseInt(input.value, 10)) || Number.parseInt(input.value) < Number.parseInt(input.min)
     }
     let button = document.getElementById("generate") as HTMLButtonElement;
-    drawMaze(generateMaze(20, 20), 5, canvas);
-    
+    let height = 20;
+    let width = 20;
+    let state = {
+        iter: primIter(Graph.random(width, height)),
+        id: -1,
+    }
+
+    state.id = setInterval(() => {
+        let step = state.iter.next(); 
+        if (!step.done) 
+            drawMaze(step.value, height, width, 5, canvas);
+        else
+            clearInterval(state.id);
+    }, 1);
+
     inputs.width.onchange = () => button.disabled = inputs.validate(inputs.width);
     inputs.height.onchange = () => button.disabled = inputs.validate(inputs.height);
     inputs.scale.onchange = () => button.disabled = inputs.validate(inputs.scale);
@@ -17,7 +31,20 @@ function run() {
         let width = Number.parseInt(inputs.width.value);
         let height = Number.parseInt(inputs.height.value);
         let scale = Number.parseInt(inputs.scale.value);
-        drawMaze(generateMaze(width, height), scale, canvas);
+        clearInterval(state.id);
+        if (!inputs.live.checked){
+            drawMaze(prim(Graph.random(width, height)), width, height, scale, canvas);
+        }
+        else {
+            state.iter = primIter(Graph.random(width, height));
+            state.id = setInterval(() => {
+                let step = state.iter.next(); 
+                if (!step.done) 
+                    drawMaze(step.value, height, width, scale, canvas);
+                else
+                    clearInterval(state.id);
+            }, 1);
+        }
     }
 
 }
